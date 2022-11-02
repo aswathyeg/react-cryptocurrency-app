a6c9687f6709dd699c1fff7507cb558e417571d8
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-11-02 15:14:25 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { Box } from "@mui/material";
 
 function App() {
-  return <div className="App">Cryptocurrency</div>;
+  return (
+    <Box width="400 px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+      </Routes>
+    </Box>
+  );
 }
 
 export default App;
