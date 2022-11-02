f11c74fd9f479c36d607732d16ecf3a36c0be2fb
aswathyeg <aswathy.eg.bijesh@gmail.com>
2022-11-02 15:22:36 +0530

    added basic components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import "./App.css";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Box } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import ExerciseDetail from "./pages/ExerciseDetail";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,8 +13,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/exercise/:id" element={<ExerciseDetails />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
       </Routes>
+      <Footer />
     </Box>
   );
 }
